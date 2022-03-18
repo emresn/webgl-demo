1b8b26428eb271c08e9a2414b1913488c9ce77cd
Emre Sönmez <esonmez357@gmail.com>
2022-03-18 16:18:37 +0300

    style : rows

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,13 @@
 import {
   Environment,
+  Html,
   OrbitControls,
   TransformControls,
+  useProgress,
 } from '@react-three/drei';
-import { Html, useProgress } from '@react-three/drei';
 import { Canvas, useLoader } from '@react-three/fiber';
 import React, { Suspense, useState } from 'react';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
-import Button from '@/components/buttons/Button';
 
 import { allObjects } from '@/model/character';
 
@@ -16,36 +15,47 @@ const HomePage = () => {
   const [selectedIndex, setSelectedIndex] = useState(0);
 
   return (
-    <div className='bg-gray-800 flex flex-col items-center justify-center min-h-screen min-w-screen'>
-      <div className='flex h-20 items-center justify-center text-white w-full'>
-        <h3> three.js playground</h3>
+    <div className='bg-zinc-800 flex flex-col items-start min-h-screen min-w-screen'>
+      <div className='justify-center p-4 text-center text-white w-full'>
+        <h3> WebGL Demo</h3>
       </div>
-      <div className='bg-gray-300 container flex-auto h-96 m-0 rounded-3xl'>
-        <Canvas
-          camera={{ position: [0, 0, 5], fov: 30 }}
-          className='h-96 rounded-3xl'
-        >
-          <ambientLight intensity={0.1} />
-          <directionalLight color='red' position={[0, 0, 5]} />
+      <div className='flex flex-auto flex-row items-stretch w-full'>
+        <div className='flex flex-col gap-4 items-center py-4 text-gray-50 w-1/6'>
+          <div className='text-center w-full'>
+            <h4>Models</h4>
+            <hr className='mx-2' />
+          </div>
+          {allObjects.map((e, idx) => (
+            <button
+              className={`${
+                selectedIndex == idx && 'text-lime-300'
+              } font-medium text-lg hover:text-gray-300`}
+              onClick={() => setSelectedIndex(idx)}
+              key={idx}
+            >
+              {e.name}
+            </button>
+          ))}
+        </div>
+        <div className='flex-auto p-4'>
+          <Canvas
+            camera={{ position: [0, 0, 5], fov: 30 }}
+            className='rounded-3xl'
+          >
+            <ambientLight intensity={0.1} />
+            <directionalLight color='red' position={[0, 0, 5]} />
 
-          <TransformControls mode='translate'>
-            <mesh>
-              <Suspense fallback={<Loader />}>
-                <Test url={allObjects[selectedIndex].href} />
-                <OrbitControls />
-                <Environment preset='sunset' background />
-              </Suspense>
-            </mesh>
-          </TransformControls>
-        </Canvas>
-      </div>
-      <div className='flex h-20 items-center justify-center w-full'>
-        <Button
-          onClick={() => setSelectedIndex(selectedIndex === 0 ? 1 : 0)}
-          variant='primary'
-        >
-          Change Object
-        </Button>
+            <TransformControls mode='translate'>
+              <mesh>
+                <Suspense fallback={<Loader />}>
+                  <Test url={allObjects[selectedIndex].href} />
+                  <OrbitControls />
+                  <Environment preset='sunset' background />
+                </Suspense>
+              </mesh>
+            </TransformControls>
+          </Canvas>
+        </div>
       </div>
     </div>
   );
